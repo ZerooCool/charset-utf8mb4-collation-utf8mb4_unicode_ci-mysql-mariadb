781bb2d5fbbef9a309a24d0ad20d249d4e510992
Zer00CooL <g2pc@visionduweb.com>
2019-10-06 22:05:01 +0200

    Update utf8-utf8mb4.sh

--- utf8-utf8mb4.sh
+++ utf8-utf8mb4.sh
@@ -1,35 +1,57 @@
 #!/bin/bash
 
+######################################
+# Identifiants de la base de données #
+######################################
 database='database'
 user='user'
 pass='pass'
 
+######################
+# Le nouvel encodage #
+######################
 charset='utf8mb4'
 collate='utf8mb4_unicode_ci'
 
-echo "Changer l'encodage / charset de la base de données: $database"
+###################################
+# Appliqué sur la base de données #
+###################################
+echo "Changer l'encodage / charset de la base de données : $database"
 mysql -u $user -p$pass $database -s -e "ALTER DATABASE $database CHARACTER SET = $charset COLLATE = $collate;"
 
+###########################
+# Appliqué sur les tables #
+###########################
 for table in $(mysql $database -s --skip-column-names -e 'show tables')
 do
   echo ''
-  echo "Changing charset of table: $table"
+  echo "Changer l'encodage / charset de la table : $table"
   mysql -u $user -p$pass $database -s -e "ALTER TABLE $table CHARACTER SET $charset COLLATE $collate"
 
-  echo "Converting charset of table: $table"
-  
+  echo "Convertir l'encodage / charset de la table : $table"
   SET FOREIGN_KEY_CHECKS=0;
-  
   mysql -u $user -p$pass $database -s -e "ALTER TABLE $table CONVERT TO CHARACTER SET $charset COLLATE $collate"
 done
 
+#######################
+# Manque les colonnes #
+#######################
+
+###
+###
+###
+
 echo ''
-echo 'Conversion done!'
+echo 'Conversion effectuée !'
 echo ''
-echo 'Optimizing tables...'
+
+#############################
+# Réparation / Optimisation #
+#############################
+echo 'Début de l'optimisation des tables !'
 echo ''
 
 mysqlcheck -u $user -p$pass $database --auto-repair --optimize
 
 echo ''
-echo 'Done! Have a nice day! ;)'
+echo 'Optimisation terminée !'

bf208d68a6968b76781c9956bd8e2a8a8a12a4f6
Zer00CooL <g2pc@visionduweb.com>
2019-10-07 00:14:25 +0200

    Create base_table_colonnes.sh

---
+++ base_table_colonnes.sh
@@ -0,0 +1,22 @@
+ #!/bin/bash
+ # mycollate.sh <database> [<charset> <collation>]
+ # changes MySQL/MariaDB charset and collation for one database - all tables and
+ # all columns in all tables
+ 
+ DB="test_intermediaire"
+ CHARSET="utf8mb4"
+ COLL="utf8mb4_unicode_ci"
+ 
+ [ -n "$DB" ] || exit 1
+ [ -n "$CHARSET" ] || CHARSET="utf8mb4"
+ [ -n "$COLL" ] || COLL="utf8mb4_unicode_ci"
+ 
+ echo $DB
+ echo "ALTER DATABASE \`$DB\` CHARACTER SET $CHARSET COLLATE $COLL;" | mysql
+ 
+ echo "USE \`$DB\`; SHOW TABLES;" | mysql -s | (
+     while read TABLE; do
+         echo $DB.$TABLE
+         echo "ALTER TABLE \`$TABLE\` CONVERT TO CHARACTER SET $CHARSET COLLATE $COLL;" | mysql $DB
+     done
+ )
